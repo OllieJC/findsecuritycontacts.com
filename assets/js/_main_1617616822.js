4503990c76781ed1f8857c8f1cda9a0b25da2aff
OllieJC <oliver.chalk@outlook.com>
2021-04-05 11:30:40 +0100

    Drop extra JS files

---
+++ assets/js/_main_1617616822.js
@@ -0,0 +1,35 @@
+if (window.location.pathname == "/query" && window.history.replaceState) {
+  try {
+    var urlParams = new URLSearchParams(window.location.search);
+    var raw_domain = urlParams.get("domain").toLowerCase();
+    raw_domain = raw_domain.replace("%3a%2f%2f", "://");
+    if (raw_domain.indexOf("://") == -1) {
+      raw_domain = `http://${raw_domain}`;
+    }
+    var tmp = new URL(raw_domain);
+    if (tmp) {
+      window.history.replaceState({}, "gotsecuritytxt.com", `/query/${tmp.hostname}`);
+    }
+  } catch (e) {
+    console("Failed to rewrite URL: ", e);
+  }
+}
+
+function toggleMenu(evt) {
+  var targetStr = evt.currentTarget.attributes.getNamedItem("data-bs-target").value;
+  var target = document.getElementById(targetStr.replace("#", ""));
+  if (target.className.indexOf("show") != -1) {
+    target.classList.remove("show");
+  } else {
+    target.classList.add("show");
+  }
+}
+
+try {
+  var navBars = document.getElementsByClassName("navbar-toggler");
+  for (var i = 0; i < navBars.length; i++) {
+    navBars[i].addEventListener("click", toggleMenu, false);
+  }
+} catch (e) {
+  console.log("Unable to setup menu:", e);
+}
